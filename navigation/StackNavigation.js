f19520504e175dca2179cbb5db2b91573d6abb6d
ahmed Abd El-Halim <37662016+ahmedHalim2290@users.noreply.github.com>
2020-05-08 21:00:20 +0200

    Initial commit

---
+++ navigation/StackNavigation.js
@@ -0,0 +1,38 @@
+import {  createAppContainer, createStackNavigator } from 'react-navigation';
+
+import TabNavigation from './TabNavigation';
+import Decks from '../screens/Decks';
+import Deck from '../screens/Deck';
+import AddDeck from '../screens/AddDeck';
+import AddCard from '../screens/AddCard';
+import Quiz from '../screens/Quiz';
+
+const StackNavigator = createStackNavigator({
+  Primary: {
+    screen: TabNavigation,
+    navigationOptions: {
+      header: null
+    }
+  },
+  Decks: {
+    screen: Decks
+  },
+  Deck: {
+    screen: Deck
+  },
+  AddDeck: {
+    screen: AddDeck
+  },
+  AddCard: {
+    screen: AddCard
+  },
+  Quiz: {
+    screen: Quiz
+  }
+});
+
+const StackNavigation = createAppContainer(StackNavigator);
+
+export default StackNavigation;
+
+
---
+++ utils/colors.js
@@ -0,0 +1,9 @@
+export const white = '#fff';
+export const textColor = '#333';
+export const textRed = '#dc3545';
+
+export const bgBlue = '#FF9600';
+export const bgBlueLight = '#0090ff';
+export const bgGreen = '#4BD75F';
+
+export const black ='black';
